3a3995765ef2fe4b3898b400c08e6499e999b099
mscobblepot <tomecsko.reka@gmail.com>
2022-03-29 13:41:51 +0200

    Modifies hole position

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 function positionHoleRandomly() {
     let hole = document.getElementById("hole");
     hole.addEventListener("animationiteration", () => {
-        const max = 950;
-        const min = 550;
+        const max = 57 * window.innerHeight / 100;
+        const min = 97 * window.innerHeight / 100;
         const randomTop = Math.floor(Math.random() * (max-min) + min);
         hole.style.top = `-${randomTop}px`;
     })

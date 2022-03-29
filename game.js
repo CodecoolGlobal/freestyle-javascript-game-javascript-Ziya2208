d55de5991ba0e4f610365bf906f7bcc04f0be83a
mscobblepot <tomecsko.reka@gmail.com>
2022-03-29 13:34:36 +0200

    Randomizes hole position

--- game.js
+++ game.js
@@ -1,7 +1,17 @@
-initGame();
+function positionHoleRandomly() {
+    let hole = document.getElementById("hole");
+    hole.addEventListener("animationiteration", () => {
+        const max = 950;
+        const min = 550;
+        const randomTop = Math.floor(Math.random() * (max-min) + min);
+        hole.style.top = `-${randomTop}px`;
+    })
+}
 
 function initGame() {
-
+    positionHoleRandomly()
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
+
+initGame();
